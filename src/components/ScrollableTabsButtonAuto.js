e823b247ab7a595b2d2174a93e48c71e44abca60
OkkyYukiya <mangaokky@icloud.com>
2021-01-31 06:31:11 +0900

    Tabs added.

---
+++ src/components/ScrollableTabsButtonAuto.js
@@ -0,0 +1,107 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Box from "@material-ui/core/Box";
+
+import News from "./News";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`scrollable-auto-tabpanel-${index}`}
+      aria-labelledby={`scrollable-auto-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box mt={6}>
+          <div>{children}</div>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `scrollable-auto-tab-${index}`,
+    "aria-controls": `scrollable-auto-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    width: "100%",
+    backgroundColor: "#f5f5f5",
+  },
+}));
+
+const ScrollableTabsButtonAuto = () => {
+  const classes = useStyles();
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState("business");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed" color="default">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="scrollable"
+          scrollButtons="auto"
+          aria-label="scrollable auto tabs example"
+        >
+          <Tab label="business" {...a11yProps(0)} />
+          <Tab label="technology" {...a11yProps(1)} />
+          <Tab label="general" {...a11yProps(2)} />
+          <Tab label="health" {...a11yProps(3)} />
+          <Tab label="science" {...a11yProps(4)} />
+          <Tab label="entertainment" {...a11yProps(5)} />
+          <Tab label="sports" {...a11yProps(6)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        <News category={category} />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <News category={category} />
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        Item Four
+      </TabPanel>
+      <TabPanel value={value} index={4}>
+        Item Five
+      </TabPanel>
+      <TabPanel value={value} index={5}>
+        Item Six
+      </TabPanel>
+      <TabPanel value={value} index={6}>
+        Item Seven
+      </TabPanel>
+    </div>
+  );
+};
+
+export default ScrollableTabsButtonAuto;
