0285c58ee7e87bda8016920b55dad3be7c085920
OkkyYukiya <mangaokky@icloud.com>
2021-03-19 14:22:22 +0900

    add Auth global state

---
+++ src/Store/AuthProvider.js
@@ -0,0 +1,61 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../firebase";
+
+const AuthContext = createContext();
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
+
+  const signup = async (email, password, username) => {
+    const authUser = await auth.createUserWithEmailAndPassword(email, password);
+    await authUser.user?.updateProfile({ displayName: username });
+  };
+
+  const login = async (email, password) => {
+    await auth.signInWithEmailAndPassword(email, password);
+  };
+
+  const logout = () => {
+    auth.signOut();
+  };
+
+  const resetPassword = (email) => {
+    return auth.sendPasswordResetEmail(email);
+  };
+
+  const updateEmail = (email) => {
+    return currentUser.updateEmail(email);
+  };
+
+  const updatePassword = (password) => {
+    return currentUser.updatePassword(password);
+  };
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+    signup,
+    login,
+    logout,
+    resetPassword,
+    updateEmail,
+    updatePassword,
+  };
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
